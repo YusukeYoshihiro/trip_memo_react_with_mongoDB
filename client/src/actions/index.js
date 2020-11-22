0fb3d78aa915147707aa4145e968d58567d51402
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-11-21 17:18:40 -0800

    modified client & port(server)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,41 @@
 import axios from 'axios';
 import { FETCH_USER } from "./types";
 
+// Thunk middleware
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
     dispatch({type: FETCH_USER, payload: res.data});
 };
+
+// React-Redux rule
+// Return pure Object（Action)
+
+// export const fetchUser = () => {
+
+//     return {
+//         type: FETCH_USER, 
+//         payload: res.data
+//     }
+// }
+
+
+// const incrementIfOdd = () => (dispatch) => dispatch({type: FETCH_USER, payload: res.data});
+
+
+
+
+
+
+// function test () {
+//     return function async(dispatch) {
+//         return value
+//     }
+// }
+
+
+// const test = () =>  {
+//   return const dispatch = ()=>{
+//       return 
+//   }
+// }
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 
+// combineReducers() can be used to organize multiple reducers
+// In this case, there is one reducer but it also can use
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
 })
